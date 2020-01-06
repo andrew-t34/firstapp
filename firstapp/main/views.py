5c28d05284e38309ab2df3faaf443e701cc46549
andrew-t34 <a.khomenko83@mail.ru>
2020-01-07 01:10:41 +0300

    Second commit

--- firstapp/main/views.py
+++ firstapp/main/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    text = 'А это переданная переменная'
-    return render(request, 'index.html', {'text': text})
+    text = 'Здравствуй Владислав. Ты ЖОПА'
+    daddy = 'А. Папа говно'
+    return render(request, 'index.html', {'text': text,'daddy': daddy })
